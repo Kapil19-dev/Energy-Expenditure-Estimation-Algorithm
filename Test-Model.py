dbe74a94d1a150f65e132cce1d57fba4952a4bfd
RyanLefebvre <ral1036@wildcats.unh.edu>
2020-01-26 10:53:06 -0500

    Created templates for scripts needed for analysis.

---
+++ Test-Model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Ryan Lefebvre 1/26/2020
+"""
+
+
+def main():
+    print("Hello World from Test-Model!")
+    
+##Tell python to run main function 
+if __name__ == "__main__":
+    main()
---
+++ Train-Model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Ryan Lefebvre 1/26/2020
+"""
+
+
+def main():
+    print("Hello World from Train-Model!")
+    
+##Tell python to run main function 
+if __name__ == "__main__":
+    main()
