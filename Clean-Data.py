2aedbf5ca8b1f3588bbf7c1c83c7b0e7e6027f1c
RyanLefebvre <ral1036@wildcats.unh.edu>
2020-01-27 10:59:57 -0500

    Used pandas to open raw data and drop unwanted rows from dataset.

--- Clean-Data.py
+++ Clean-Data.py
@@ -2,10 +2,20 @@
 """
 Ryan Lefebvre 1/26/2020
 """
+import pandas as pd
 
+# opens raw data.csv and 'cleans' the data so it 
+# can be used for analysis to create energy expenditure 
+# estimation model
+def cleanData():
+    rawData = pd.read_csv("data/DLW_TDEE_DATA.csv")
+    # only 767 rows of data but read_csv returns 1467 rows
+    # drop other 700 rows + 1 row for headers to only retain 
+    # rows of interest
+    rawData.drop( rawData.tail(701).index ,inplace=True )
 
 def main():
-    print("Hello World from Clean-Data!")
+    cleanData()
     
 ##Tell python to run main function 
 if __name__ == "__main__":
